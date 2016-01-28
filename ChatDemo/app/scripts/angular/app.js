c075e106ef7489d87ab8229d8e2f921e121a07b8
winddyhe <hgplan@126.com>
2016-01-29 02:08:57 +0800

    angularjs tutorials part6, services.
    angularjs教程part6，服务

--- ChatDemo/app/scripts/angular/app.js
+++ ChatDemo/app/scripts/angular/app.js
@@ -15,41 +15,95 @@ rAppModule.controller('UserController', function($scope){
     }
 });
 
-rAppModule.controller('PlayerController', ['$scope', function($scope){ 
-    $scope.player = {};
-    $scope.player.playing = false;
-    $scope.player.audio = document.createElement('audio');
-    $scope.player.audio.src = '/media/1.aac'
+var apiKey = 'MDIyMzY1NDg5MDE0NTM5MTQ4NzhjNjhlMw000',
+    nprUrl = 'http://api.npr.org/query?id=61&fields=relatedLink,title,byline,text,audio,image,pullQuote,all&output=JSON';
     
-    $scope.player.play = function()
+rAppModule.controller('PlayerController', ['$scope', 'nprService', 'player', function($scope, nprService, player){ 
+    $scope.player = player;
+       
+    $scope.GetAllArticles = function()
     {
-      $scope.player.audio.play();
-      $scope.player.playing = true;  
-    };
+        $scope.player = player;
+        nprService.programs(apiKey).success(function (data, status) {
+            $scope.programs = data.list.story;
+        });
+    }
     
-    $scope.player.stop = function()
+    $scope.ClearAllArticles = function()
     {
-        $scope.player.audio.pause();
-        $scope.player.playing = false;
-    };
+        $scope.programs = null;
+    }
+}]);
+
+rAppModule.controller('RelatedController', ['$scope', 'player', function($scope, player){
+    $scope.player = player;
     
-    $scope.player.audio.addEventListener('ended', function()
-    {
-       $scope.$apply(function(){
-           $scope.player.stop();
-       });
+    $scope.$watch('player.current', function(program){
+       if (program){
+          $scope.related = [];
+          angular.forEach(program.relatedLink, function(link){
+             $scope.related.push({
+                 link: link.link[0].$text,
+                 caption: link.caption.$text
+             });
+          });
+       } 
     });
 }]);
 
-rAppModule.controller('RelatedController', ['$scope', function($scope){
-    
+rAppModule.factory('audio', ['$document', function($document){
+    var audio = $document[0].createElement('audio');
+    return audio;
 }]);
 
+rAppModule.factory('player', ['audio', '$rootScope', function(audio, $rootScope){
+    var player = {
+        playing: false,
+        current: null,
+        progress:0,
+        ready: false,
+        
+        play: function(program){
+            if (player.playing) player.stop();
+            var url = program.audio[0].format.mp4.$text; 
+            player.current = program;
+            audio.src = url;
+            audio.play();
+            player.playing = true;
+        },
+        stop: function(){
+            if (player.playing) audio.pause();
+            player.ready = player.playing = false;
+            player.current = null;
+        },
+        currentTime: function() {
+            return audio.currentTime;
+        },
+        currentDuration: function() {
+            return audio.duration;
+        }
+    }
 
-var apiKey = 'MDIyMzY1NDg5MDE0NTM5MTQ4NzhjNjhlMw000',
-    nprUrl = 'http://api.npr.org/query?id=61&fields=relatedLink,title,byline,text,audio,image,pullQuote,all&output=JSON';
+    audio.addEventListener('canplay', function (evt) {
+        $rootScope.$apply(function () {
+            player.ready = true;
+        });
+    });
+    audio.addEventListener('timeupdate', function(evt){
+       $rootScope.$apply(function(){
+         player.progress = player.currentTime();
+         player.progress_percent = player.progress / player.currentDuration();  
+       });
+    });
+    audio.addEventListener('ended', function(){
+       $rootScope.$apply(function(){ 
+           player.stop();
+       }); 
+    });
+    return player;
+}]);
 
-rAppModule.controller('DataBindController', function($scope, $http)
+rAppModule.controller('DataBindController', ['$scope', function($scope)
 {
     $scope.persion = { name: "Winddy He" };
     var updateClock = function()
@@ -73,30 +127,20 @@ rAppModule.controller('DataBindController', function($scope, $http)
     {
         $scope.counter -= amount;
     }
-    
-    $scope.GetAllArticles = function()
-    {
-        $scope.audio = document.createElement('audio');;
-        
-        $scope.audio.src = 'http://pd.npr.org/npr-mp4/npr/sf/2013/07/20130726_sf_05.mp4?orgId=1&topicId=1032&ft=3&f=61';
-        $scope.audio.play();
-    
-        $http({
-            method: 'JSONP',
-            url: nprUrl + '&apiKey=' + apiKey + '&callback=JSON_CALLBACK'
-        }).success(function (data, status) {
-            $scope.programs = data.list.story;
-        }).error(function (data, status, header, config) {
-            
-        });
+}]);
+
+rAppModule.factory('nprService', ['$http', function($http) {
+    var doRequest = function(apiKey) {
+      return $http({
+        method: 'JSONP',
+        url: nprUrl + '&apiKey=' + apiKey + '&callback=JSON_CALLBACK'
+      });
     }
-    
-    $scope.ClearAllArticles = function()
-    {
-        $scope.programs = null;
-        $scope.audio.pause();
-    }
-});
+ 
+    return {
+      programs: function(apiKey) { return doRequest(apiKey); }
+    };
+}]);
 
 rAppModule.directive('nprLink', function(){
     return {
@@ -105,7 +149,7 @@ rAppModule.directive('nprLink', function(){
         replace:true,
         scope:{
             ngModel: '=',
-            play: '&',
+            player: '=',
         },
         templateUrl:'/views/tpl/nprListItem.html',
         link: function(scope, ele, attr){
@@ -113,3 +157,31 @@ rAppModule.directive('nprLink', function(){
         }
     }
 });
+
+rAppModule.factory('githubService', ['$http', function($http)
+{   
+   var doRequest = function(username, path) {
+       return $http({
+           method: 'JSONP',
+           url: 'https://api.github.com/users/' + username + '/' + path + '?callback=JSON_CALLBACK'
+       });
+   }
+   
+   return { events: function(username){ return doRequest(username, 'events'); }}; 
+}]);
+
+rAppModule.controller('ServiceController', ['$scope', '$timeout', 'githubService',
+    function ($scope, $timeout, githubService) {
+        var timeout;
+        $scope.$watch('username', function (newUsername) {
+            if (newUsername) {
+                if (timeout) $timeout.cancel(timeout);
+                timeout = $timeout(function () {
+                    githubService.events(newUsername)
+                    .success(function (data, status, headers) {
+                        $scope.events = data.data;
+                    });
+                });
+            }
+        });
+    }]);
